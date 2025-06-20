4c5e78acd31923e1e09f518686ee6d839b25ed4e
kimkwangcheol <kkc3379@gmail.com>
2025-06-21 02:19:13 +0900

    커밋완료

---
+++ mysql-asrahandb/src/test/kotlin/kr/ac/kumoh/ce/s20220099/mysql_asrahandb/MysqlAsrahandbApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20220099.mysql_asrahandb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlAsrahandbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20220099/mysql_asrahandb/MysqlAsrahandbApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20220099.mysql_asrahandb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlAsrahandbApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlAsrahandbApplication>(*args)
+}
